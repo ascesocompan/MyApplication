b6bd989827d7469a13a77c014a4f8bd2ed8e06e5
Mac1 <antonydotcom@hotmail>
2018-01-13 00:46:40 -0600

    Comprobacion

--- app/src/main/java/com/example/mac1/myapplication/MainActivity.java
+++ app/src/main/java/com/example/mac1/myapplication/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
- //Actualizacion HOLASIQUETAL
+ //Actualizacion HOLASIQUETAL MALDITA SEA AGARRA
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
