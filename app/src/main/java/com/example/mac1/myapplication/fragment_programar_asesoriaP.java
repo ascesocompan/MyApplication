6e1d410f548f86725db618580e65cb071e38a030
Mac1 <antonydotcom@hotmail>
2018-01-18 02:10:05 -0600

    Se añade ventana y fragment

--- app/src/main/java/com/example/mac1/myapplication/fragment_programar_asesoriaP.java
+++ app/src/main/java/com/example/mac1/myapplication/fragment_programar_asesoriaP.java
@@ -8,9 +8,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -47,17 +49,34 @@ public class fragment_programar_asesoriaP extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        Spinner materias;
+        final Spinner materias;
         final View view = inflater.inflate(R.layout.fragment_fragment_programar_asesoria, container,
                 false);
         materias = (Spinner)view.findViewById(R.id.spinner);
+
         ArrayAdapter adapter =  ArrayAdapter.createFromResource(getActivity(),R.array.Opciones,R.layout.support_simple_spinner_dropdown_item);
          adapter.setDropDownViewResource(android.R.layout.simple_list_item_1);
-
-
-
         materias.setAdapter(adapter);
+
+        materias.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                    if(position>=0){
+                        final Toast toast = null;
+                        toast.makeText(getContext(), "Usted selecciono  : " + materias.getSelectedItem(), Toast.LENGTH_SHORT).show();
+                    }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
         return view;
+
+
     }
+
+
 
 }
